6dde90599223ece2dc667802f767be43fc8c6478
AndriEnd <ndrian1710@gmail.com>
2023-08-01 00:34:40 +0700

    update graddle

--- app/src/main/java/com/example/aplikasi_iot/MainActivity.kt
+++ app/src/main/java/com/example/aplikasi_iot/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.aplikasi_iot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
